94fda617facf75c1a7bef8f4348c4127487b6636
Merlin Volkmer <merlin.volkmer@outlook.com>
2021-04-25 14:28:38 +0200

    Added the gamemode select ui

---
+++ app/src/main/java/com/se2/bopit/ui/GamemodeSelectActivity.java
@@ -0,0 +1,16 @@
+package com.se2.bopit.ui;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.se2.bopit.R;
+
+public class GamemodeSelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gamemode_select);
+
+    }
+
+}
