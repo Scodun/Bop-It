02321c1582cc6aef6d9fb5ae3f50c3c388f1f884
Merlin Volkmer <merlin.volkmer@outlook.com>
2021-04-27 22:58:42 +0200

    Randomized SimpleTextButtonMiniGame

--- app/src/main/java/com/se2/bopit/ui/games/SimpleTextButtonMiniGame.java
+++ app/src/main/java/com/se2/bopit/ui/games/SimpleTextButtonMiniGame.java
@@ -4,12 +4,41 @@ import com.se2.bopit.domain.ButtonModel;
 import com.se2.bopit.domain.GameModel;
 import com.se2.bopit.ui.ButtonMiniGameFragment;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class SimpleTextButtonMiniGame extends ButtonMiniGameFragment {
+
+    private static final ArrayList<ButtonModel> buttonModels = initializeButtonModels();
+
+    private static int numberButtons = 3;
+
     public SimpleTextButtonMiniGame() {
-        super(new GameModel<>("Click BOP !",
-                new ButtonModel("BOP"),
-                new ButtonModel("NOPE"),
-                new ButtonModel("OOP"),
-                new ButtonModel("HOP")));
+        super(createGameModel());
+    }
+
+    private static GameModel<ButtonModel> createGameModel() {
+        Collections.shuffle(buttonModels);
+
+        ArrayList<ButtonModel> wrongResponses = new ArrayList<>();
+        for (int i=1; i<numberButtons; i++) {
+            wrongResponses.add(buttonModels.get(i));
+        }
+
+        return new GameModel<>(
+                String.format("Select %s!", buttonModels.get(0).label),
+                buttonModels.get(0),
+                wrongResponses
+        );
+    }
+
+    private static ArrayList<ButtonModel> initializeButtonModels() {
+        ArrayList<ButtonModel> buttonModelsTmp = new ArrayList<>();
+        buttonModelsTmp.add(new ButtonModel("BOP"));
+        buttonModelsTmp.add(new ButtonModel("NOP"));
+        buttonModelsTmp.add(new ButtonModel("OOP"));
+        buttonModelsTmp.add(new ButtonModel("HOP"));
+        buttonModelsTmp.add(new ButtonModel("MOP"));
+        return buttonModelsTmp;
     }
 }
