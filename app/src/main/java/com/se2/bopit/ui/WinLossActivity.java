3eb27e29313b6891a45c19071e524e9b7b5f884d
calgara12 <michael.polzer@gmx.net>
2021-04-27 17:49:38 +0200

    Add Show Score to WinLossActivity

--- app/src/main/java/com/se2/bopit/ui/WinLossActivity.java
+++ app/src/main/java/com/se2/bopit/ui/WinLossActivity.java
@@ -2,16 +2,23 @@ package com.se2.bopit.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.se2.bopit.R;
 
 public class WinLossActivity extends AppCompatActivity {
-
-    public int[] Points;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_win_loss);
+        Intent intent = getIntent();
+        showScore(intent.getIntExtra("score",100));
+    }
+
+    private void showScore(Integer score) {
+        TextView tv_score =(TextView) findViewById(R.id.tv_score);
+        tv_score.setText("Score: " + score);
     }
 }
