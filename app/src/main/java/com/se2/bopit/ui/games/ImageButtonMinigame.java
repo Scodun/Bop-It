659f4a1a7a387ec9162465ef0b3badc9b3a7c6e0
marknapp <maknapp@edu.aau.at>
2021-04-28 17:02:43 +0200

    Created Class ImageButtonMinigame

---
+++ app/src/main/java/com/se2/bopit/ui/games/ImageButtonMinigame.java
@@ -0,0 +1,13 @@
+package com.se2.bopit.ui.games;
+
+import androidx.fragment.app.Fragment;
+import com.se2.bopit.domain.interfaces.GameListener;
+import com.se2.bopit.domain.interfaces.MiniGame;
+
+public class ImageButtonMinigame extends Fragment implements MiniGame {
+
+    GameListener listener;
+    @Override
+    public void setGameListener(GameListener listener) {
+    }
+}
