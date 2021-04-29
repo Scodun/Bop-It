7266ca15c795cb785b75d88cfc64e10f347b382e
Raphael Burgstaller <scodusoft@gmail.com>
2021-04-29 17:05:16 +0200

    fix: code smells

--- app/src/androidTest/java/com/se2/bopit/ui/GameActivityTest.java
+++ app/src/androidTest/java/com/se2/bopit/ui/GameActivityTest.java
@@ -5,7 +5,6 @@ import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
-import com.se2.bopit.R;
 
 import org.junit.After;
 import org.junit.Before;
@@ -13,13 +12,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.junit.Assert.*;
 
 
 @RunWith(AndroidJUnit4.class)
