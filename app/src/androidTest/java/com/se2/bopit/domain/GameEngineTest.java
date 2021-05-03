e925b2166fdc86cc392b4b01faf32f451ae46aac
Raphael Burgstaller <scodusoft@gmail.com>
2021-05-03 13:37:17 +0200

    fix: hemcrest error fix: codesmells

--- app/src/androidTest/java/com/se2/bopit/domain/GameEngineTest.java
+++ app/src/androidTest/java/com/se2/bopit/domain/GameEngineTest.java
@@ -7,9 +7,13 @@ import androidx.test.filters.SmallTest;
 import com.se2.bopit.domain.interfaces.GameEngineListener;
 import com.se2.bopit.domain.interfaces.MiniGame;
 
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.awaitility.Awaitility.await;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.concurrent.Callable;
 
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 
@@ -17,40 +21,41 @@ import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentat
 @SmallTest
 public class GameEngineTest  {
 
+    private boolean isGameEnd=false;
     @Test
     public void startNewGame() {
         getInstrumentation().runOnMainSync(() -> {
             GameEngine engine = new GameEngine();
-            engine.setGameEngineListener(gameEngineListener);
+            engine.setGameEngineListener(new GameEngineListener() {
+                @Override
+                public void onGameEnd(int score) {
+                    isGameEnd=true;
+                    assertEquals(0,score);
+                }
+
+                @Override
+                public void onScoreUpdate(int score) {
+                    fail();
+                }
+
+                @Override
+                public void onGameStart(MiniGame game, long time) {
+                    assertNotNull(game);
+                    assertTrue(time>0);
+                }
+
+                @Override
+                public void onTimeTick(long time) {
+                    assertTrue(time>0);
+                }
+            });
             engine.startNewGame();
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
         });
+        await().atMost(5, SECONDS).until(onGameEnd());
+        assertTrue(isGameEnd);
     }
 
-    private final GameEngineListener gameEngineListener = new GameEngineListener() {
-        @Override
-        public void onGameEnd(int score) {
-            assertEquals(score,0);
-        }
-
-        @Override
-        public void onScoreUpdate(int score) {
-            fail();
-        }
-
-        @Override
-        public void onGameStart(MiniGame game, long time) {
-            assertNotNull(game);
-            assertTrue(time>0);
-        }
-
-        @Override
-        public void onTimeTick(long time) {
-            assertTrue(time>0);
-        }
-    };
+    private Callable<Boolean> onGameEnd() {
+        return () -> isGameEnd;
+    }
 }
--- app/src/androidTest/java/com/se2/bopit/ui/GameActivityTest.java
+++ app/src/androidTest/java/com/se2/bopit/ui/GameActivityTest.java
@@ -14,6 +14,7 @@ import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static org.junit.Assert.assertNotNull;
 
 
 @RunWith(AndroidJUnit4.class)
@@ -35,6 +36,7 @@ public class GameActivityTest {
 
     @Test
     public void onBackPressed() {
+        assertNotNull(activityRule);
         Espresso.pressBack();
         intended(hasComponent(GamemodeSelectActivity.class.getName()));
     }
