d72bb8a97a5a63da40df82db6ad781d954c33969
Ramiz Ismailov <ramiz.ismailov@skiline.cc>
2021-05-04 16:30:08 +0200

    Add MiniGameMock
    Improve domain testability #35

---
+++ app/src/test/java/com/se2/bopit/domain/mock/MiniGameMock.java
@@ -0,0 +1,14 @@
+package com.se2.bopit.domain.mock;
+
+import com.se2.bopit.domain.interfaces.GameListener;
+import com.se2.bopit.domain.interfaces.MiniGame;
+
+public class MiniGameMock implements MiniGame {
+    // public fields to enable manipulations from tests
+    public GameListener listener;
+
+    @Override
+    public void setGameListener(GameListener listener) {
+        this.listener = listener;
+    }
+}
