026662f285450ad50bc185c88b2a2faa23773349
marknapp <maknapp@edu.aau.at>
2021-05-05 15:55:26 +0200

    get random combination in textview

--- app/src/main/java/com/se2/bopit/ui/games/RightButtonCombination.java
+++ app/src/main/java/com/se2/bopit/ui/games/RightButtonCombination.java
@@ -1,9 +1,8 @@
 package com.se2.bopit.ui.games;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,9 +12,16 @@ import com.se2.bopit.R;
 import com.se2.bopit.domain.interfaces.GameListener;
 import com.se2.bopit.domain.interfaces.MiniGame;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class RightButtonCombination extends Fragment implements MiniGame {
 
     GameListener listener;
+
+    boolean result;
+    boolean result2;
 
     RightButtonCombination(){
         super(R.layout.fragment_right_button_combination_game);
@@ -24,6 +30,7 @@ public class RightButtonCombination extends Fragment implements MiniGame {
     @Override
     public void setGameListener(GameListener listener) {
         this.listener = listener;
+        this.listener = null;
     }
 
     @Override
@@ -32,6 +39,27 @@ public class RightButtonCombination extends Fragment implements MiniGame {
         getView().findViewById(R.id.pressRight).setOnClickListener(clickHandler);
         getView().findViewById(R.id.pressLeft).setOnClickListener(clickHandler);
         getView().findViewById(R.id.pressUp).setOnClickListener(clickHandler);
+
+        String n = "";
+
+        List<String> partOne = new ArrayList<>();
+        partOne.add("RIGHT");
+        partOne.add("LEFT");
+        partOne.add("DOWN");
+        partOne.add("UP");
+        List<String> partTwo = new ArrayList<>();
+        partOne.add("RIGHT");
+        partOne.add("LEFT");
+        partOne.add("DOWN");
+        partOne.add("UP");
+
+        Collections.shuffle(partOne);
+        Collections.shuffle(partTwo);
+
+        n = partOne.get(1)+" "+partTwo.get(0);
+
+        TextView textView = getView().findViewById(R.id.textView2);
+        textView.setText(n);
 
     }
 
