72cbcdc6073bc46ec62b6a35a3a2f61248755045
marknapp <maknapp@edu.aau.at>
2021-05-12 17:41:12 +0200

    implemented method createRandomModel()
    in ImageButtonMinigameModel

--- app/src/main/java/com/se2/bopit/domain/ImageButtonMinigameModel.java
+++ app/src/main/java/com/se2/bopit/domain/ImageButtonMinigameModel.java
@@ -1,10 +1,37 @@
 package com.se2.bopit.domain;
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ImageButtonMinigameModel extends GameModel<ImageButtonModel>{
 
     public ImageButtonMinigameModel(String challenge, ImageButtonModel correctResponse, List<ImageButtonModel> wrongResponses) {
         super(challenge, correctResponse, wrongResponses);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public static ImageButtonMinigameModel createRandomModel(){
+
+        List<ImageButtonModel> possibleAnswers = Arrays.stream(ButtonImage.values())
+                .map(ImageButtonModel::new)
+                .collect(Collectors.toList());
+
+        Collections.shuffle(possibleAnswers);
+
+        ImageButtonModel correctResponse = possibleAnswers.get(0);
+
+        List<ImageButtonModel> wrongAnswers = possibleAnswers.subList(1,possibleAnswers.size());
+
+        return new ImageButtonMinigameModel(
+                String.format("Select the %s", correctResponse),
+                correctResponse,
+                wrongAnswers
+        );
     }
 }
