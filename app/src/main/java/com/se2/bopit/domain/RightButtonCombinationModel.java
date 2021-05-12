b0bc54b37a5e06cea2f0bd7040d19a9f5187a98a
marknapp <maknapp@edu.aau.at>
2021-05-12 19:01:51 +0200

    implemented method createRandomModel()
    in RightButtonCombinationModel

--- app/src/main/java/com/se2/bopit/domain/RightButtonCombinationModel.java
+++ app/src/main/java/com/se2/bopit/domain/RightButtonCombinationModel.java
@@ -1,6 +1,14 @@
 package com.se2.bopit.domain;
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RightButtonCombinationModel extends GameModel<RightButtonModel> {
 
@@ -10,5 +18,35 @@ public class RightButtonCombinationModel extends GameModel<RightButtonModel> {
                                        RightButtonModel secondCorrectResponse, List<RightButtonModel> wrongAnswers) {
         super(challenge, correctResponse, wrongAnswers);
         this.secondCorrectResponse = secondCorrectResponse;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public static RightButtonCombinationModel createRandomModel(){
+
+        List<RightButtonModel> possibleAnswers = Arrays.stream(RightButton.values())
+                .map(RightButtonModel::new)
+                .collect(Collectors.toList());
+
+        Collections.shuffle(possibleAnswers);
+
+        RightButtonModel firstCorrectResponse = possibleAnswers.get(0);
+
+        Collections.shuffle(possibleAnswers);
+
+        RightButtonModel secondCorrectResponse = possibleAnswers.get(0);
+
+        List<RightButtonModel> wrongResponses = new ArrayList<>();
+
+        for(RightButtonModel rightButtonModel : possibleAnswers){
+            if(rightButtonModel != firstCorrectResponse && rightButtonModel != secondCorrectResponse){
+                wrongResponses.add(rightButtonModel);
+            }
+        }
+        return new RightButtonCombinationModel(
+                firstCorrectResponse.label+" "+secondCorrectResponse.label,
+                firstCorrectResponse,
+                secondCorrectResponse,
+                wrongResponses
+        );
     }
 }
