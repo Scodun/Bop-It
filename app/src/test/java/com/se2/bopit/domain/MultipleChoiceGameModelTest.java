1ab1fee9d20c5b313be88a0be9d7d3e583d6886c
Ramiz Ismailov <ramiz.ismailov@skiline.cc>
2021-05-16 21:23:54 +0200

    Fix SensorMiniGameModelTest
    Cover light sensor minigame (optional) #64

--- app/src/test/java/com/se2/bopit/domain/MultipleChoiceGameModelTest.java
+++ app/src/test/java/com/se2/bopit/domain/MultipleChoiceGameModelTest.java
@@ -1,5 +1,9 @@
 package com.se2.bopit.domain;
 
+import com.se2.bopit.domain.interfaces.GameListener;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.LinkedList;
@@ -8,15 +12,43 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.verify;
 
 public class MultipleChoiceGameModelTest {
+
+    MultipleChoiceGameModel<ButtonModel> gameModel;
+    ButtonModel correct;
+    ButtonModel[] wrongs;
+    private GameListener gameListenerMock;
+
+    @Before
+    public void setUp() {
+        correct = new ButtonModel("correct");
+        wrongs = new ButtonModel[3];
+        for(int i = 0; i < 3; i++) {
+            wrongs[i] = new ButtonModel("wrong" + i);
+        }
+        gameModel = new ButtonMiniGameModel("test", correct, wrongs);
+        gameListenerMock = mock(GameListener.class);
+        gameModel.setGameListener(gameListenerMock);
+    }
+
+    @After
+    public void tearDown() {
+        reset(gameListenerMock);
+    }
+
     @Test
     public void shouldSetCorrectFlag() {
-        MultipleChoiceGameModel<ButtonModel> gm = new ButtonMiniGameModel("test",
-                new ButtonModel("correct"),
-                new ButtonModel("wrong"));
-        assertTrue(gm.responses.stream().filter(m -> m.label.equals("correct")).findAny().get().isCorrect);
-        assertFalse(gm.responses.stream().filter(m -> m.label.equals("wrong")).findAny().get().isCorrect);
+        assertTrue(gameModel.responses.stream()
+                .filter(m -> m == correct)
+                .allMatch(m -> m.isCorrect));
+        assertTrue(gameModel.responses.stream()
+                .filter(m -> m.label.startsWith("wrong"))
+                .noneMatch(m -> m.isCorrect));
     }
 
     @Test
@@ -31,5 +63,19 @@ public class MultipleChoiceGameModelTest {
         LinkedList<ButtonModel> wrongs = new LinkedList<>(gm.responses);
         wrongs.remove(correct);
         assertNotEquals(original, wrongs);
+    }
+
+    @Test
+    public void handleCorrectResponse() {
+
+        assertTrue(gameModel.handleResponse(correct));
+        verify(gameListenerMock).onGameResult(eq(true));
+
+    }
+
+    @Test
+    public void handleWrongResponse() {
+        assertFalse(gameModel.handleResponse(wrongs[0]));
+        verify(gameListenerMock).onGameResult(eq(false));
     }
 }
