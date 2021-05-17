32995f4f83017777185f98679304d8df289e5b18
rijstc <clebner@edu.aau.at>
2021-05-17 17:50:47 +0200

    fixed playing music when backpress

--- app/src/main/java/com/se2/bopit/ui/GamemodeSelectActivity.java
+++ app/src/main/java/com/se2/bopit/ui/GamemodeSelectActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.se2.bopit.R;
 import com.se2.bopit.domain.services.BackgroundSoundService;
 
@@ -15,13 +17,14 @@ public class GamemodeSelectActivity extends AppCompatActivity {
             localMultiplayerButton,
             onlineMultiplayerButton;
     private ImageButton settingsButton;
-
+    private Intent music;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gamemode_select);
-        startService(new Intent(this, BackgroundSoundService.class));
+        music = new Intent(this, BackgroundSoundService.class);
+        startService(music);
 
         initializeButtons();
         initializeListeners();
@@ -50,10 +53,10 @@ public class GamemodeSelectActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onBackPressed()
-    {
+    public void onBackPressed() {
         super.onBackPressed();
         this.finishAffinity();
+        stopService(music);
     }
 
 }
