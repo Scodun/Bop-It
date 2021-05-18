fe0987c80c64b58bcba7d5e78c9fcd75b9324fc5
Ramiz Ismailov <ramiz.ismailov@skiline.cc>
2021-05-18 09:02:18 +0200

    Add GameRulesTest
    Cover light sensor minigame (optional) #64

---
+++ app/src/test/java/com/se2/bopit/domain/GameRulesTest.java
@@ -0,0 +1,59 @@
+package com.se2.bopit.domain;
+
+import com.se2.bopit.ui.games.ColorButtonMiniGame;
+import com.se2.bopit.ui.games.CoverLightSensorMiniGame;
+import com.se2.bopit.ui.games.WeirdTextButtonMiniGame;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameRulesTest {
+
+    static final Class<?>[] TEST_GAMES = {
+            ColorButtonMiniGame.class,
+            CoverLightSensorMiniGame.class,
+            WeirdTextButtonMiniGame.class};
+
+    GameRules gameRules;
+
+    @Before
+    public void setUp() throws Exception {
+        gameRules = new GameRules(TEST_GAMES);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void resetToDefault() {
+        gameRules.resetToDefault();
+    }
+
+    @Test
+    public void disablePermanently() {
+        gameRules.getItems().forEach(i -> assertTrue(i.available));
+        gameRules.disablePermanently(CoverLightSensorMiniGame.class);
+        gameRules.getItems().forEach(i -> {
+            if (i.type == CoverLightSensorMiniGame.class) {
+                assertFalse(i.available);
+                assertFalse(i.enabled);
+            } else {
+                assertTrue(i.available);
+            }
+        });
+    }
+
+    @Test
+    public void getItems() {
+        assertEquals(3, gameRules.getItems().size());
+    }
+
+    @Test
+    public void getEnabledItems() {
+        assertEquals(2, gameRules.getEnabledItems().size());
+    }
+}
