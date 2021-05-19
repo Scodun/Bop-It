b4cdca68ba96f6a5bc62b1c157db97136173c316
marknapp <maknapp@edu.aau.at>
2021-05-19 19:16:24 +0200

    added VolumeButtonGameModel class in domain

---
+++ app/src/main/java/com/se2/bopit/domain/VolumeButtonGameModel.java
@@ -0,0 +1,37 @@
+package com.se2.bopit.domain;
+
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class VolumeButtonGameModel extends MultipleChoiceGameModel<VolumeButtonModel> {
+
+    protected VolumeButtonGameModel(String challenge, VolumeButtonModel correctResponse, List<VolumeButtonModel> wrongResponses) {
+        super(challenge, correctResponse, wrongResponses);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public static VolumeButtonGameModel createRandomModel() {
+
+        List<VolumeButtonModel> possibleAnswers = Arrays.stream(VolumeButton.values())
+                .map(VolumeButtonModel::new)
+                .collect(Collectors.toList());
+
+        Collections.shuffle(possibleAnswers);
+
+        VolumeButtonModel correctResponse = possibleAnswers.get(0);
+
+        List<VolumeButtonModel> wrongResponses = possibleAnswers.subList(1,possibleAnswers.size());
+
+        return new VolumeButtonGameModel(
+                String.format("Press volume %s", correctResponse.label),
+                correctResponse,
+                wrongResponses);
+
+    }
+}
