20c3aa2e6921c0e6f4dbefa603c87d776b904d4a
marknapp <maknapp@edu.aau.at>
2021-05-19 19:16:02 +0200

    added VolumeButtonModel class in domain

---
+++ app/src/main/java/com/se2/bopit/domain/VolumeButtonModel.java
@@ -0,0 +1,12 @@
+package com.se2.bopit.domain;
+
+public class VolumeButtonModel extends ResponseModel{
+
+    public VolumeButton volumeButton;
+    public String label;
+
+    public VolumeButtonModel(VolumeButton volumeButton){
+        this.volumeButton = volumeButton;
+        this.label = volumeButton.name().toUpperCase();
+    }
+}
