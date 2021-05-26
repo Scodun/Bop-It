44177dd5280707f07391a7281b07f1915d6f79a1
rijstc <clebner@edu.aau.at>
2021-05-26 21:01:31 +0200

    added boolean cheated and cheatDetected to User.java

--- app/src/main/java/com/se2/bopit/domain/models/User.java
+++ app/src/main/java/com/se2/bopit/domain/models/User.java
@@ -7,10 +7,14 @@ public class User implements Serializable {
     private final String id;
     private final String name;
     private int score = 0;
+    private boolean cheated;
+    private boolean cheatDetected;
 
     public User(String id, String name) {
         this.id = id;
         this.name = name;
+        cheated = false;
+        cheatDetected = false;
     }
 
     public void addScore() {
@@ -28,5 +32,21 @@ public class User implements Serializable {
 
     public int getScore() {
         return score;
+    }
+
+    public boolean hasCheated(){
+        return cheated;
+    }
+
+    public void setCheated(boolean cheated){
+        this.cheated = cheated;
+    }
+
+    public boolean isCheatDetected() {
+        return cheatDetected;
+    }
+
+    public void setCheatDetected(boolean cheatDetected){
+        this.cheatDetected = cheatDetected;
     }
 }
