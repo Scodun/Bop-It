f72595409834393c43d22a586417abdcbd1a87f3
Ramiz Ismailov <ramiz.ismailov@skiline.cc>
2021-06-01 10:37:22 +0200

    Apply changes to GameEngineDataProvider to the SinglePlayerGameEngineDataProvider
    Local network multiplayer gamemode #84

--- app/src/main/java/com/se2/bopit/data/SinglePlayerGameEngineDataProvider.java
+++ app/src/main/java/com/se2/bopit/data/SinglePlayerGameEngineDataProvider.java
@@ -16,14 +16,9 @@ public class SinglePlayerGameEngineDataProvider implements GameEngineDataProvide
 
     static final String TAG = SinglePlayerGameEngineDataProvider.class.getSimpleName();
 
-    final GameEngine client;
-    final GameEngineServer server;
+    GameEngine client;
+    GameEngineServer server;
 
-    public SinglePlayerGameEngineDataProvider(GameEngine client, GameEngineServer server) {
-        this.client = client;
-        this.server = server;
-
-    }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
@@ -39,9 +34,9 @@ public class SinglePlayerGameEngineDataProvider implements GameEngineDataProvide
     }
 
     @Override
-    public int sendGameResult(String userId, boolean result, ResponseModel responseModel) {
+    public void sendGameResult(String userId, boolean result, ResponseModel responseModel) {
         Log.d(TAG, "sendGameResult: " + userId + ", " + result + ", " + responseModel);
-        return server.sendGameResult(userId, result, responseModel);
+        server.sendGameResult(userId, result, responseModel);
     }
 
     @Override
@@ -59,4 +54,15 @@ public class SinglePlayerGameEngineDataProvider implements GameEngineDataProvide
     public User[] getRoundResult() {
         return server.getRoundResult();
     }
+
+    @Override
+    public void setGameEngineClient(GameEngine client) {
+        this.client = client;
+    }
+
+    @Override
+    public void setGameEngineServer(GameEngineServer server) {
+        this.server = server;
+    }
+
 }
