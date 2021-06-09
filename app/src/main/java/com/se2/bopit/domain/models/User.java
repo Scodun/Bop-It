3e11da7389afa24a299a99526ecf250565c3d83c
calgara12 <michael.polzer@gmx.net>
2021-06-09 20:24:26 +0200

    Add Ready bool to User

--- app/src/main/java/com/se2/bopit/domain/models/User.java
+++ app/src/main/java/com/se2/bopit/domain/models/User.java
@@ -7,10 +7,16 @@ public class User implements Serializable {
     private final String id;
     private final String name;
     private int score = 0;
+    private boolean ready = false;
 
     public User(String id, String name) {
         this.id = id;
         this.name = name;
+    }
+    public User(String id, String name, boolean ready) {
+        this.id = id;
+        this.name = name;
+        this.ready = ready;
     }
 
     public void addScore() {
@@ -21,12 +27,17 @@ public class User implements Serializable {
         return id;
     }
 
-
     public String getName() {
         return name;
     }
 
+    public boolean isReady(){return ready;}
+
     public int getScore() {
         return score;
+    }
+
+    public void setReady(boolean ready){
+        this.ready = ready;
     }
 }
