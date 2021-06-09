a3a7b8757c3412e28c36172f3e85ce8c4fd6c0a7
marknapp <maknapp@edu.aau.at>
2021-06-09 15:47:57 +0200

    added listeners to the new buttons
    -> googleplay achievements button
    -> achievements button

--- app/src/main/java/com/se2/bopit/ui/GamemodeSelectActivity.java
+++ app/src/main/java/com/se2/bopit/ui/GamemodeSelectActivity.java
@@ -2,22 +2,34 @@ package com.se2.bopit.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
 import android.widget.ImageButton;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.games.Games;
+import com.se2.bopit.BuildConfig;
 import com.se2.bopit.R;
+
+import java.util.Objects;
 
 import info.hoang8f.widget.FButton;
 
 public class GamemodeSelectActivity extends BaseActivity {
 
+    private ActivityResultLauncher<Intent> activityResultLauncher;
+
     private FButton
             singleplayerButton,
             localMultiplayerButton,
-            onlineMultiplayerButton;
-    private ImageButton settingsButton;
+            onlineMultiplayerButton,
+            googlePlayAchievements;
+    private ImageButton
+            settingsButton,
+            achievementButton;
     private ConstraintLayout customRulesButton;
 
 
@@ -28,6 +40,7 @@ public class GamemodeSelectActivity extends BaseActivity {
 
         initializeButtons();
         initializeListeners();
+        initActivityLauncher();
     }
 
 
@@ -37,6 +50,9 @@ public class GamemodeSelectActivity extends BaseActivity {
         onlineMultiplayerButton = findViewById(R.id.onlineMultiplayerButton);
         settingsButton = findViewById(R.id.settingsButton);
         customRulesButton = findViewById(R.id.customRulesButton);
+        achievementButton = findViewById(R.id.achievments);
+
+        googlePlayAchievements = findViewById(R.id.googlePlayAchievements);
     }
 
     private void initializeListeners() {
@@ -54,6 +70,25 @@ public class GamemodeSelectActivity extends BaseActivity {
         settingsButton.setOnClickListener(v -> startActivity(new Intent(this, SettingsActivity.class)));
 
         customRulesButton.setOnClickListener(v -> startActivity(new Intent(this, CustomizeGameRulesActivity.class)));
+
+        achievementButton.setOnClickListener(v -> startActivity(new Intent(this, AchievementsSelectActivity.class)));
+
+        googlePlayAchievements.setOnClickListener(onAchievements);
+
+    }
+    private final View.OnClickListener onAchievements = v -> {
+        if (!BuildConfig.DEBUG && GoogleSignIn.getLastSignedInAccount(this) != null) {
+            Games.getAchievementsClient(this, Objects.requireNonNull(GoogleSignIn.getLastSignedInAccount(this)))
+                    .getAchievementsIntent()
+                    .addOnSuccessListener(intent -> activityResultLauncher.launch(intent));
+        }
+    };
+
+    private void initActivityLauncher() {
+        activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                });
     }
 
     @Override
