0b3e1d0473190676c5e5bec843ecc49097e5cf2c
rijstc <clebner@edu.aau.at>
2021-06-22 23:46:39 +0200

    refactoring

--- app/src/main/java/com/se2/bopit/ui/RulesActivity.java
+++ app/src/main/java/com/se2/bopit/ui/RulesActivity.java
@@ -1,21 +1,12 @@
 package com.se2.bopit.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.ScrollView;
 
 import com.se2.bopit.R;
 
 public class RulesActivity extends BaseActivity {
-    private ScrollView svRules;
-    private LinearLayout linLayout;
     private ImageView ivBullet1;
     private ImageView ivBullet2;
     private ImageView ivBullet3;
